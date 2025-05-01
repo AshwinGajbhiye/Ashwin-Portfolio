a1e8ba04169cbf7f3c6e9f76dcce4f99242040b8
AshwinGajbhiye <ashwingajbhiye36@gmail.com>
2025-05-01 15:18:51 +0530

    did some minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Adrian JSM
+          Ashwin Gajbhiye
         </a>
 
         <nav className="desktop">
